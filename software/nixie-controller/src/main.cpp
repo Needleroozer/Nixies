de5e8d0bb55a5b4b7f55a7cde11956373935bf66
Needleroozer <Needleroozer@users.noreply.github.com>
2024-02-25 19:37:40 -0500

    Create functional multiplexing routine

--- software/nixie-controller/src/main.cpp
+++ software/nixie-controller/src/main.cpp
@@ -1,12 +1,63 @@
 #include<Arduino.h>
 
+// PB0 is bit 2
+// PB1 is bit 1
+// PB2 is bit 0
+#define ENABLE_ANODE(num) (PORTB = (PORTB & ~0x07) | ((num & 0b01) << 2) | (num & 0b010) | ((num & 0b0100) >> 2))
+
+// PA0 is bit 0
+// PA1 is bit 3
+// PA2 is bit 1
+// PA3 is bit 2
+#define ENABLE_CATHODE(num) (PORTA = (PORTA & ~0xF0) | ((num & 0b0100) << 1) | ((num & 0b010) << 1) | ((num & 0b1000) >> 2) | (num & 0b01))
+
+# define DISABLE_CATHODES() (PORTA &= 0x50) 
+
+// decimal points are active-high since we're using NPN transistors
+void set_dp(uint8_t val)
+{
+    switch(val)
+    {
+        case 1:
+            PORTA |= (1 << PA5); // RHDP
+            break;
+    	case 2:
+            PORTA |= (1 << PA7); // LHDP
+            break;
+        case 3:
+            PORTA |= (1 << PA5) | (1 << PA7);
+            break;
+        default:
+            PORTA &= ~((1 << PA5) | (1 << PA7));
+    }
+}
+
 void setup()
 {
-    DDRA |= 1 << 5; //LED on PA5
+    // 1 is output, 0 is input for the DDR* registers
+    DDRA |= 0xEF; // PA7, PA6, PA5, PA3:0
+
+    DDRB |= 0x07; // PB2:0
+
+    DISABLE_CATHODES(); // To keep anything from turning on we make sure all the cathodes are off
+
+    ENABLE_ANODE(4); // This is a little jank. Ideally we'd be able to set this to output 8 or 9
+                     // since that doesn't energize anything (or set it to 10+ for output blanking)
+                     // but we don't have the high-bit input line wired to the microcontroller.
+                     // Since this board only has four digits soldered on, enabling digit 4 is perfectly safe
 }
 
 void loop()
 {
-    PORTA ^= 1<<5;
-    delay(1000);
+    uint8_t numbers[] = {7, 3, 3, 1};
+    for (int i=0; i<4; ++i)
+    {
+        ENABLE_ANODE(i);
+        ENABLE_CATHODE(numbers[i]);
+        set_dp(i%4);
+        delayMicroseconds(200);
+
+        //prevent ghosting onto the next number
+        DISABLE_CATHODES(); 
+    }
 }
